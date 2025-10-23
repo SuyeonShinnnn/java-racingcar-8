59f8487798824c110497acf647cae4df48140358
SuyeonShinnnn <suyeons5536@gmail.com>
2025-10-23 14:19:03 +0900

    feat: 경주할 자동차 이름 입력 기능 추가

---
+++ src/main/java/racingcar/Racing.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class Racing {
+    private List<String> cars;
+
+    public Racing() {
+        cars = new ArrayList<>();
+    }
+
+    public void runRacing() {
+        String input = readLine();
+    }
+}
