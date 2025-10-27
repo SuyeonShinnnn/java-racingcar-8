79acc60e41ee4f732499dbb857596046de513112
SuyeonShinnnn <suyeons5536@gmail.com>
2025-10-27 17:06:39 +0900

    rename: 테스트 코드 파일명 변경

--- src/test/java/racingcar/CarNameInputTest.java
+++ src/test/java/racingcar/CarNameInputTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class RacingTest {
+public class CarNameInputTest {
     private Racing racing;
 
     @BeforeEach
